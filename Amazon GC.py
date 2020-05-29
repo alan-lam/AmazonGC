0c38207aa5d3573d87ccfb7e76b9e828b14e1af0
alan-lam <alanlam00@gmail.com>
2020-05-29 13:24:05 -0700

    update

---
+++ Amazon GC.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+df = pd.read_csv('https://docs.google.com/spreadsheets/d/1lec9VOr8N3kHUlUFCbAdO3PKGhk_i62Myw_B6N4c3jk/export?gid=0&format=csv', names=['Source', 'Date', 'Amount'])
+groups = df.groupby('Source')
+
+# fig, ax = plt.subplots(figsize=(20,5))
+# for name, group in groups:
+    # ax.plot(pd.to_datetime(group['Date']), group['Amount'], marker='o', linestyle='', label=name)
+# ax.legend()
+# plt.show()
+
+total_by_group = df.groupby('Source').sum()
+
+total = df['Amount'].sum()
+
+f = open('README.md', 'w')
+f.write('# AmazonGC\n\n')
+f.write(f'{total_by_group.to_string()}\n\n')
+f.write(f'${total}\n\n')
+f.write(f'https://stackoverflow.com/questions/19611729/getting-google-spreadsheet-csv-into-a-pandas-dataframe\n')
+f.write(f'https://stackoverflow.com/questions/21654635/scatter-plots-in-pandas-pyplot-how-to-plot-by-category\n')
+f.write(f'https://stackoverflow.com/questions/38256750/make-a-scatter-plot-in-matplotlib-with-dates-on-x-axis-and-values-on-y\n')
+f.close()
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # AmazonGC
 
-https://stackoverflow.com/questions/19611729/getting-google-spreadsheet-csv-into-a-pandas-dataframe  
-https://stackoverflow.com/questions/21654635/scatter-plots-in-pandas-pyplot-how-to-plot-by-category  
-https://stackoverflow.com/questions/38256750/make-a-scatter-plot-in-matplotlib-with-dates-on-x-axis-and-values-on-y  
+                   Amount
+Source                   
+Branded Surveys     70.24
+Microsoft Rewards  280.00
+Swagbucks          120.00
+
+$470.24
+
+https://stackoverflow.com/questions/19611729/getting-google-spreadsheet-csv-into-a-pandas-dataframe
+https://stackoverflow.com/questions/21654635/scatter-plots-in-pandas-pyplot-how-to-plot-by-category
+https://stackoverflow.com/questions/38256750/make-a-scatter-plot-in-matplotlib-with-dates-on-x-axis-and-values-on-y
